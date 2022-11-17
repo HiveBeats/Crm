4b13c36a8fcbf2021930f9e53e58a7514595851e
ivan_laletin <ivan.laletin@fonedynamics.com.au>
2022-11-17 23:36:50 +0400

    feat: bootstrapper

---
+++ Crm.Client.Avalonia/Bootstrapper.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Crm.Client.Application.Clients;
+using Crm.Client.ViewModel.Clients;
+using Crm.Client.ViewModel.Common;
+using Splat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crm.Client.Avalonia;
+public static class Bootstrapper
+{
+    public static void InjectServices()
+    {   
+        var configuration = new MapperConfiguration(cfg =>
+        {
+            cfg.CreateMap<Domain.Models.Client, ClientDto>();
+        });
+        var mapper = new Mapper(configuration);
+        Locator.CurrentMutable.RegisterConstant<IMapper>(new Mapper(configuration));
+        Locator.CurrentMutable.RegisterConstant<IClientService>(new MockClientService());
+    }
+
+    public static void InjectViewModels()
+    {
+        Locator.CurrentMutable.RegisterConstant(new LoginViewModel());
+        Locator.CurrentMutable.RegisterConstant(new ClientsViewModel());
+    }
+}
--- Crm.Client.Avalonia/ViewModels/MainWindowViewModel.cs
+++ Crm.Client.Avalonia/ViewModels/MainWindowViewModel.cs
@@ -16,24 +16,18 @@ public class MainWindowViewModel : ViewModelBase
     public MainWindowViewModel()
     {
         Locator.CurrentMutable.RegisterConstant(this);
-        ResolveAutomapper();
-        Locator.CurrentMutable.RegisterConstant<IClientService>(new MockClientService());
-        Locator.CurrentMutable.RegisterConstant(new LoginViewModel());
-        Locator.CurrentMutable.RegisterConstant(new ClientsViewModel());
+        
+        Bootstrapper.InjectServices();
+        Bootstrapper.InjectViewModels();
         CurrentViewModel = Locator.Current.GetService<ClientsViewModel>();
     }
 
-    private void ResolveAutomapper()
-    {
-        var configuration = new MapperConfiguration(cfg =>
-        {
-            cfg.CreateMap<Domain.Models.Client, ClientDto>();
-        });
-        var mapper = new Mapper(configuration);
-
-        Locator.CurrentMutable.RegisterConstant<IMapper>(new Mapper(configuration));
-    }
+    
     public string Greeting => "Welcome to Avalonia!";
 
-    public Crm.Client.ViewModel.Common.ViewModelBase CurrentViewModel { get => _currentViewModel; set => this.RaiseAndSetIfChanged(ref _currentViewModel, value); }
+    public Crm.Client.ViewModel.Common.ViewModelBase CurrentViewModel 
+    { 
+        get => _currentViewModel; 
+        set => this.RaiseAndSetIfChanged(ref _currentViewModel, value); 
+    }
 }
