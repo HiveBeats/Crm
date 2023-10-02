86e100d88e931ef2775098d78cb702e4229ef5db
gubanovmv <m_gubanov@bk.ru>
2023-10-02 16:08:39 +0300

    ClientDto: сменил ReactiveObject на ObservableObject

--- Crm.Client.ViewModel/Clients/ClientDto.cs
+++ Crm.Client.ViewModel/Clients/ClientDto.cs
@@ -4,13 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Crm.Client.ViewModel.Clients;
-public class ClientDto: ReactiveObject
+public class ClientDto: ObservableObject
 {
     private string _name;
     private string _contact;
 
-    public string Name { get { return _name; } set => this.RaiseAndSetIfChanged(ref _name, value); }
-    public string Contact { get => _contact; set => this.RaiseAndSetIfChanged(ref _contact, value); }   
+    public string Name { get { return _name; } set => SetProperty(ref _name, value); }
+    public string Contact { get => _contact; set => SetProperty(ref _contact, value); }   
 }
